2e8a1062502e76e5297de6ee73e193dcd6e54e84
Júlio Lopes <julio.lopes@outsystems.com>
2020-04-28 19:00:18 +0100

    Create parse method to handle multiple types

--- index.js
+++ index.js
@@ -6,35 +6,54 @@ const { Octokit } = require("@octokit/action");
 main();
 
 async function main() {
-  try {
-    ``;
-    const octokit = new Octokit();
-    const { query, ...variables } = getAllInputs();
+    try {
+        ``;
+        const octokit = new Octokit();
+        const { query, ...variables } = getAllInputs();
 
-    core.info(query);
-    for (const [name, value] of Object.entries(variables)) {
-      core.info(`> ${name}: ${value}`);
+        core.info(query);
+        for (const [name, value] of Object.entries(variables)) {
+            core.info(`> ${name}: ${value}`);
+        }
+
+        const time = Date.now();
+        const data = await octokit.graphql(query, variables);
+
+        core.info(`< 200 ${Date.now() - time}ms`);
+
+        core.setOutput("data", JSON.stringify(data, null, 2));
+    } catch (error) {
+        core.debug(inspect(error));
+        core.setFailed(error.message);
     }
+}
 
-    const time = Date.now();
-    const data = await octokit.graphql(query, variables);
+function parseValue(value) {
+	try {
+		if (parseInt(value, 10)) {
+			return parseInt(value, 10);
+		}
 
-    core.info(`< 200 ${Date.now() - time}ms`);
+		var obj = JSON.parse(value);
+		if (obj && typeof obj === "object") {
+			// this condition prevents the returning wring value
+			// because neither JSON.parse(false) or JSON.parse(1234) throw errors
+			// and JSON.parse(null) returns null so we have to check for that
+			return obj;
+		}
+	} catch (e) {
+		// ignore the error thrown by the parsing
+	}
 
-    core.setOutput("data", JSON.stringify(data, null, 2));
-  } catch (error) {
-    core.debug(inspect(error));
-    core.setFailed(error.message);
-  }
+	return value;
 }
 
 function getAllInputs() {
-  return Object.entries(process.env).reduce((result, [key, value]) => {
-    if (!/^INPUT_/.test(key)) return result;
+    return Object.entries(process.env).reduce((result, [key, value]) => {
+        if (!/^INPUT_/.test(key)) return result;
 
-    const inputName = key.substr("INPUT_".length).toLowerCase();
-    result[inputName] = value;
-    result[inputName] = result[inputName] == parseInt(result[inputName], 10) ? parseInt(result[inputName], 10) : result[inputName];
-    return result;
-  }, {});
+        const inputName = key.substr("INPUT_".length).toLowerCase();
+        result[inputName] = parseValue(value);
+        return result;
+    }, {});
 }
